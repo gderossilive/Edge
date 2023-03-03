b6e4b97f072d6e76b3a9eeb41fec7488fc1c62a4
Gianluca De Rossi <gderossi@live.com>
2023-03-03 10:07:08 +0100

    step 3b introduced

---
+++ Files/Step2c-ArcForKubernetes.ps1
@@ -0,0 +1,168 @@
+<#
+  Sample script to deploy AksEdge via Arc Enabled Servers - Custom Script Extenstion
+#>
+param(
+    [Switch] $UseK8s,
+    [string] $Tag,
+    [string] $SubscriptionName,
+    [string] $SubscriptionId,
+    [string] $TenantId,
+    [string] $ResourceGroupName,
+    [string] $ServicePrincipalName,
+    [string] $Location,
+    [string] $CustomLocationOID,
+    [string] $ServicePrincipalId,
+    [string] $Password
+)
+#Requires -RunAsAdministrator
+New-Variable -Name gAksEdgeRemoteDeployVersion -Value "1.0.230221.1000" -Option Constant -ErrorAction SilentlyContinue
+if (! [Environment]::Is64BitProcess) {
+    Write-Host "Error: Run this in 64bit Powershell session" -ForegroundColor Red
+    exit -1
+}
+Push-Location $PSScriptRoot
+$installDir = "C:\AksEdgeScript"
+$productName = "AKS Edge Essentials - K3s"
+$networkplugin = "flannel"
+if ($UseK8s) {
+    $productName ="AKS Edge Essentials - K8s"
+    $networkplugin = "calico"
+}
+
+# Here string for the json content
+$aideuserConfig = @"
+{
+    "SchemaVersion": "1.1",
+    "Version": "1.0",
+    "AksEdgeProduct": "$productName",
+    "AksEdgeProductUrl": "",
+    "Azure": {
+        "SubscriptionName": "$SubscriptionName",
+        "SubscriptionId": "$SubscriptionId",
+        "TenantId": "$TenantId",
+        "ResourceGroupName": "$ResourceGroupName",
+        "ServicePrincipalName": "$ServicePrincipalName",
+        "Location": "$Location",
+        "CustomLocationOID":"$CustomLocationOID",
+        "Auth":{
+            "ServicePrincipalId":"$ServicePrincipalId",
+            "Password":"$Password"
+        }
+    },
+    "AksEdgeConfigFile": "aksedge-config.json"
+}
+"@
+
+$aksedgeConfig = @"
+{
+    "SchemaVersion": "1.5",
+    "Version": "1.0",
+    "DeploymentType": "SingleMachineCluster",
+    "Init": {
+        "ServiceIPRangeSize": 0
+    },
+    "Network": {
+        "NetworkPlugin": "$networkplugin",
+        "InternetDisabled": false
+    },
+    "User": {
+        "AcceptEula": true,
+        "AcceptOptionalTelemetry": true
+    },
+    "Machines": [
+        {
+            "LinuxNode": {
+                "CpuCount": 4,
+                "MemoryInMB": 4096,
+                "DataSizeInGB": 20
+            }
+        }
+    ]
+}
+"@
+
+###
+# Main
+###
+if (-not (Test-Path -Path $installDir)) {
+    Write-Host "Creating $installDir..."
+    New-Item -Path "$installDir" -ItemType Directory | Out-Null
+}
+
+$starttime = Get-Date
+$starttimeString = $($starttime.ToString("yyMMdd-HHmm"))
+$transcriptFile = "$PSScriptRoot\aksedgedlog-$starttimeString.txt"
+Start-Transcript -Path $transcriptFile
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+# Download the AksEdgeDeploy modules from Azure/AksEdge
+
+$url = "https://github.com/Azure/AKS-Edge/archive/main.zip"
+$zipFile = "main-$starttimeString.zip"
+$workdir = "$installDir\AKS-Edge-main"
+if (-Not [string]::IsNullOrEmpty($Tag)) {
+    $url = "https://github.com/Azure/AKS-Edge/archive/refs/tags/$Tag.zip"
+    $zipFile = "$Tag.zip"
+    $workdir = "$installDir\AKS-Edge-$tag"
+}
+Write-Host "Step 1 : Azure/AKS-Edge repo setup"
+
+if (!(Test-Path -Path "$installDir\$zipFile")) {
+    try {
+        Invoke-WebRequest -Uri $url -OutFile $installDir\$zipFile
+    } catch {
+        Write-Host "Error: Downloading Aide Powershell Modules failed" -ForegroundColor Red
+        Stop-Transcript | Out-Null
+        Pop-Location
+        exit -1
+    }
+}
+if (!(Test-Path -Path "$workdir")) {
+    Expand-Archive -Path $installDir\$zipFile -DestinationPath "$installDir" -Force
+}
+
+$aidejson = (Get-ChildItem -Path "$workdir" -Filter aide-userconfig.json -Recurse).FullName
+Set-Content -Path $aidejson -Value $aideuserConfig -Force
+$aksedgejson = (Get-ChildItem -Path "$workdir" -Filter aksedge-config.json -Recurse).FullName
+Set-Content -Path $aksedgejson -Value $aksedgeConfig -Force
+
+$aksedgeShell = (Get-ChildItem -Path "$workdir" -Filter AksEdgeShell.ps1 -Recurse).FullName
+. $aksedgeShell
+
+# Download, install and deploy AKS EE 
+Write-Host "Step 2: Download, install and deploy AKS Edge Essentials"
+# invoke the workflow, the json file already updated above.
+$retval = Start-AideWorkflow -jsonFile $aidejson
+# report error via Write-Error for Intune to show proper status
+if ($retval) {
+    Write-Host "Deployment Successful. "
+} else {
+    Write-Error -Message "Deployment failed" -Category OperationStopped
+    Stop-Transcript | Out-Null
+    Pop-Location
+    exit -1
+}
+
+Write-Host "Step 3: Connect to Arc"
+$status = Initialize-AideArc
+if ($status){
+    Write-Host "Connecting to Azure Arc"
+    if (Connect-AideArc) {
+        Write-Host "Azure Arc connections successful."
+    } else {
+        Write-Error -Message "Azure Arc connections failed" -Category OperationStopped
+        Stop-Transcript | Out-Null
+        Pop-Location
+        exit -1
+    }
+} else { Write-Host "Error: Arc Initialization failed. Skipping Arc Connection" -ForegroundColor Red }
+
+# Retrieve your service token to view your Kubernetes resources
+Get-AksEdgeManagedServiceToken
+
+$endtime = Get-Date
+$duration = ($endtime - $starttime)
+Write-Host "Duration: $($duration.Hours) hrs $($duration.Minutes) mins $($duration.Seconds) seconds"
+Stop-Transcript | Out-Null
+Pop-Location
+exit 0
